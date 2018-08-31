1e1ebc181f19d26e6c072b1dc3e0c1dbb65a6f41
Shugda <klop9054@gmail.com>
2018-08-31 21:53:23 +0300

    Good points added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <csignal>
 #include <cstdlib>
 #include <exception>
 #include <fstream>
@@ -12,29 +11,70 @@
 using namespace std;
 using namespace TgBot;
 
-typedef pair<double, double> point;
+class point
+{
+public:
+	double first;
+	double second;
+	vector<> good;
+};
 
-vector<point> places;
+vector<point> fastfood;
+vector<point> parks;
+int readDis;
 const double radius = 6371;
 const double pi = 3.141592/180;
 const string token = "648697435:AAFBxZcZf_Y9Uxt2DYm0aqa-hoNp22hAf9E";
-point meet = {55.690673, 37.499047};
+const point meet = {55.690673, 37.499047};
 
 Bot bot(token);
 
 void load()
 {
-	ifstream file("places.txt");
+	ifstream file_1("parks.txt");
+//	ifstream file_2("fastfood.txt");
 
 	double d;
-	while(file >> d)
+	while(file_1 >> d)
 	{
 		point p;
 		p.first = d;
-		file >> d;
+		file_1 >> d;
 		p.second = d;
-		places.push_back(p);
+		
+		char c;
+		while(file_1 >> c)
+		{
+			if (c == '#')
+				break;
+			else if (c == ' ')
+				continue;
+			file_1.putback(c);
+			file_1 >> d;
+			point q;
+			q.first = d;
+			file_1 >> d;
+			q.second = d;
+			p.good.push_back(q);
+		}
+
+		string s;
+		getline(file_1, s);
+
+		parks.push_back(p);
 	}
+/*
+	while(file_2 >> d)
+	{
+		string a;
+		point p;
+		p.first = d;
+		file_2 >> d;
+		p.second = d;
+		file_2 >> a;
+		fastfood.push_back(p);
+	}
+*/
 }
 
 double calcDist(point p1, point p2)
@@ -57,17 +97,28 @@ void onStart(Message::Ptr msg)
 void onNonCommandMessage(Message::Ptr msg)
 {
 	cout << "User wrote " << msg->text << endl;
-	bot.getApi().sendMessage(msg->chat->id, "Your msg is: " + msg->text);
+	bot.getApi().sendMessage(msg->chat->id, "Distance is: " + msg->text);
+	readDis = atoi(msg->text.c_str());
 }
 
-void onPlaces(Message::Ptr msg)
+void onFastfood(Message::Ptr msg)
 {
 	string text;
 
-	for (size_t i = 0; i < places.size(); ++i)
-		text += to_string(calcDist(meet, places[i])) + "\n";		
+	for (size_t i = 0; i < fastfood.size(); ++i)
+		text += to_string(calcDist(meet, fastfood[i])) + "\n";		
 
-	bot.getApi().sendMessage(msg->chat->id, "Distance to macs:\n " + text);
+	bot.getApi().sendMessage(msg->chat->id, "Distance to fastfood:\n " + text);
+}
+
+void onParks(Message::Ptr msg)
+{
+	string text;
+
+	for (size_t i = 0; i < parks.size(); ++i)
+		text += to_string(calcDist(meet, parks[i])) + "\n";		
+
+	bot.getApi().sendMessage(msg->chat->id, "Distance to parks:\n " + text);
 }
 
 string getCoords(point p)
@@ -93,45 +144,34 @@ void placesSort(vector<point> &v)
 
 void onRoute(Message::Ptr msg)
 {
-	string url = "https://www.google.ru/maps/dir/";
+	point p1 = parks[rand() % parks.size()];
+	point p2 = p1.good[rand() % p1.good.size()];
+	point p3 = p2.good[rand() % p2.good.size()];
+	point p4 = p3.good[rand() % p3.good.size()];
 
-	int n1 = rand() % places.size();
-	int n2 = rand() % places.size();
-	while (n1 == n2)
-		n2 = rand() % places.size();
-	int n3 = rand() % places.size();
-	while (n3 == n1 || n3 == n2)
-		n3 = rand() % places.size();
-	int n4 = rand() % places.size();
-	while (n4 == n1 || n4 == n2 || n4 == n3)
-		n4 = rand() % places.size();
-	int n5 = rand() % places.size();
-	while (n5 == n1 || n5 == n2 || n5 == n3 || n5 == n4)
-		n5 = rand() % places.size();
+	vector<point> coords = {meet, p1, p2, p3, p4, meet};
 
-	vector<point> coords = {meet, places[n1], places[n2], places[n3], places[n4], places[n5]};
-
-	placesSort(coords);
-
+	//dst
 	double dst = 0;
 	for (size_t i = 0; i < coords.size()-1; ++i)
 		dst += calcDist(coords[i], coords[i+1]);
-	dst += calcDist(coords[coords.size()-1], meet);
 	dst *= 1.2;
 
+	// url
+	string url = "https://www.google.ru/maps/dir/";
 	for (size_t i = 0; i < coords.size(); ++i)
 		url += getCoords(coords[i]);
-	url += getCoords(meet);
-
 	url += "data=!3m1!4b1!4m2!4m1!3e2"; // magic, don't edit
-	bot.getApi().sendMessage(msg->chat->id, "Заезд на " + to_string(int(ceil(dst))) + " км.\n" + url);
+
+	bot.getApi().sendMessage(msg->chat->id, "Заезд на " +/* to_string(int(ceil(dst))) */to_string(readDis) + " км.\n" + url);
 }
 
 int main()
 {
 	bot.getEvents().onNonCommandMessage([](Message::Ptr message) { onNonCommandMessage(message); });
 	bot.getEvents().onCommand("start", [](Message::Ptr message) { onStart(message); });
-	bot.getEvents().onCommand("places", [](Message::Ptr message) { onPlaces(message); });
+	bot.getEvents().onCommand("fastfood", [](Message::Ptr message) { onFastfood(message); });
+	bot.getEvents().onCommand("parks", [](Message::Ptr message) { onParks(message); });
 	bot.getEvents().onCommand("route", [](Message::Ptr message) { onRoute(message); });
 
 	load();
